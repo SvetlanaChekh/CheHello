5eb06f89adb74b834ed2937ed9f7136f3ac98ae3
Android <Android@NB-8CG8286G7H.sakhgu.net>
2021-11-05 22:30:41 +1100

    first commit

---
+++ app/src/main/java/com/example/mydesign/ShowActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mydesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ShowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show)
+        //val nextButton: Button=findViewById(R.id.next_button)
+        //nextButton.setOnClickListener {  }
+    }
+}
